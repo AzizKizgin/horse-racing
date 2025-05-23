193d7414962ad4e9ad737426e4c76c2441676d3d
Aziz Kızgın <azizkzgn@gmail.com>
2025-05-23 12:21:35 +0300

    feat: add ContainerButton component with props and styles

---
+++ src/components/ContainerButton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// props
+const props = defineProps<{
+  title: string
+  onStart: () => void
+}>()
+</script>
+<template>
+  <button @click="props.onStart" type="button" id="start-button" class="container-button">
+    <span class="start-button-text">
+      {{ props.title }}
+    </span>
+  </button>
+</template>
+<style scoped>
+.container-button {
+  background-color: #085451; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 12px;
+}
+</style>
